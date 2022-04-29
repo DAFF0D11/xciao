1f87304f08055cb7215a11473f67f771c4e7200f
DAFF0D11 <89229230+DAFF0D11@users.noreply.github.com>
2022-04-29 13:38:07 -0400

    Major overhaul
        Key remaps are no longer limited to a single class and single keybind.
    
        Arguments required have been reduced from 5 to 2, we only need the triggering key.
        Old: xciao "Chromium" "Control" "bracketright" "Mod1Mask" "Right"
        New: xciao "Control" "bracketright"
    
        This is because we now define the remaps internally in xciao.c remaps[]
        array (like dwm rules[])

--- xciao.c
+++ xciao.c
@@ -1,39 +1,56 @@
-/* gcc -Wall -Wextra -o xoi xoi.c -lX11 -lXtst */
-/* sxhkd ~/.config/bspwm/bspwmsxhkd */
-/* change name? xciao XCIAO */
+/* gcc -Wall -Wextra -o xciao xciao.c -lX11 -lXtst */
+
 #include <stdio.h>
 #include <string.h>
 
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 
-/* #include <X11/extensions/XTest.h> */
+#define LENGTH(X) (sizeof X / sizeof X[0])
 
-void fakepresskey(Display *display, Window win, char *typemask, char *strkey);
-char *getwindowclass(Display *display, Window w, XClassHint ch);
+typedef struct {
+  const char *class;
+  char *inputMask;
+  char *inputKey;
+  char *outputMask;
+  char *outputKey;
+} Remap;
 
-int main(int argc, char **argv) {
-  Display *display;
-  Window focus;
-  int revert;
-  XClassHint ch;
-  // Ensure we have an argument
-  if (argc != 6)
-    return 1;
+static Remap remaps[] = {
+ /*       Class      | Input mask  | input key    | Output mask  | Output key */
+    {"Brave-browser", "ControlMask", "k",           "ControlMask", "Next"}, /* Next tab */
+    {"Brave-browser", "ControlMask", "j",           "ControlMask", "Prior"},/* Previous tab */
+    {"Brave-browser", "ControlMask", "d",           "",            "Next"}, /* Page down */
+    {"Brave-browser", "ControlMask", "u",           "",            "Prior"},/* Page up */
+    {"Brave-browser", "Mod1Mask",    "h",           "",            "Left"}, /* Left arrow key */
+    {"Brave-browser", "Mod1Mask",    "j",           "",            "Down"}, /* Down arrow key */
+    {"Brave-browser", "Mod1Mask",    "k",           "",            "Up"},   /* Up arrow key */
+    {"Brave-browser", "Mod1Mask",    "l",           "",            "Right"},/* Right arrow key */
+    {"Brave-browser", "ControlMask", "bracketleft", "Mod1Mask",    "Left"}, /* Backwards history */
+    {"Brave-browser", "ControlMask", "bracketright","Mod1Mask",    "Right"},/* Forwards history */
 
-  /* xgetfocus.c - link against libX11 */
-  display = XOpenDisplay(NULL);
-  XGetInputFocus(display, &focus, &revert);
-  XGetClassHint(display, focus, &ch);
+    {"Chromium", "ControlMask", "k",           "ControlMask", "Next"}, /* Next tab */
+    {"Chromium", "ControlMask", "j",           "ControlMask", "Prior"},/* Previous tab */
+    {"Chromium", "ControlMask", "d",           "",            "Next"}, /* Page down */
+    {"Chromium", "ControlMask", "u",           "",            "Prior"},/* Page up */
+    {"Chromium", "Mod1Mask",    "h",           "",            "Left"}, /* Left arrow key */
+    {"Chromium", "Mod1Mask",    "j",           "",            "Down"}, /* Down arrow key */
+    {"Chromium", "Mod1Mask",    "k",           "",            "Up"},   /* Up arrow key */
+    {"Chromium", "Mod1Mask",    "l",           "",            "Right"},/* Right arrow key */
+    {"Chromium", "ControlMask", "bracketleft", "Mod1Mask",    "Left"}, /* Backwards history */
+    {"Chromium", "ControlMask", "bracketright","Mod1Mask",    "Right"},/* Forwards history */
 
-  if (strcmp(getwindowclass(display, focus, ch), argv[1]) == 0) {
-    fakepresskey(display, focus, argv[4],argv[5]);
-  } else {
-    fakepresskey(display, focus, argv[2],argv[3]);
-  }
-
-  return 0;
-}
+    {"firefox-nightly", "ControlMask", "k",           "ControlMask", "Next"}, /* Next tab */
+    {"firefox-nightly", "ControlMask", "j",           "ControlMask", "Prior"},/* Previous tab */
+    {"firefox-nightly", "ControlMask", "d",           "",            "Next"}, /* Page down */
+    {"firefox-nightly", "ControlMask", "u",           "",            "Prior"},/* Page up */
+    {"firefox-nightly", "Mod1Mask",    "h",           "",            "Left"}, /* Left arrow key */
+    {"firefox-nightly", "Mod1Mask",    "j",           "",            "Down"}, /* Down arrow key */
+    {"firefox-nightly", "Mod1Mask",    "k",           "",            "Up"},   /* Up arrow key */
+    {"firefox-nightly", "Mod1Mask",    "l",           "",            "Right"},/* Right arrow key */
+    {"firefox-nightly", "ControlMask", "bracketleft", "Mod1Mask",    "Left"}, /* Backwards history */
+    {"firefox-nightly", "ControlMask", "bracketright","Mod1Mask",    "Right"} /* Forwards history */
+};
 
 char *getwindowclass(Display *display, Window w, XClassHint ch) {
   XGetClassHint(display, w, &ch);
@@ -61,15 +78,41 @@ void fakepresskey(Display *display, Window win, char *typemask, char *strkey) {
   ev.xkey.keycode = XKeysymToKeycode(display, XStringToKeysym(strkey));
   ev.xkey.same_screen = True;
 
-  /* press */
   ev.xkey.type = KeyPress;
   XSendEvent(display, win, True, KeyPressMask, &ev);
   XFlush(display);
-  /* usleep(100000); */
 
-  /* release */
   ev.xkey.type = KeyRelease;
   XSendEvent(display, win, True, KeyReleaseMask, &ev);
   XFlush(display);
-  /* usleep(100000); */
+}
+
+int main(int argc, char **argv) {
+  Display *display;
+  Window focus;
+  int revert;
+  XClassHint ch;
+  unsigned int i;
+  const Remap *r;
+
+  if (argc != 3)
+    return 1;
+
+  display = XOpenDisplay(NULL);
+  XGetInputFocus(display, &focus, &revert);
+  XGetClassHint(display, focus, &ch);
+
+  for (i = 0; i < LENGTH(remaps); i++) {
+    r = &remaps[i];
+    if (strcmp(getwindowclass(display, focus, ch), r->class) == 0) {
+      if ((strcmp(r->inputMask, argv[1]) == 0) &&
+          (strcmp(r->inputKey, argv[2]) == 0)) {
+        fakepresskey(display, focus, r->outputMask, r->outputKey);
+        return 0;
+      }
+    }
+  }
+
+  fakepresskey(display, focus, argv[1], argv[2]);
+  return 0;
 }
